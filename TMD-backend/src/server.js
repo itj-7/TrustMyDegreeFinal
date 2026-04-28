4991c5d3962f953f010ab6f390b5de49db04e143
itj-7 <ba.baghdadi@ensta.edu.dz>
2026-04-28 23:51:23 +0100

    for the railway

--- TMD-backend/src/server.js
+++ TMD-backend/src/server.js
@@ -11,10 +11,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        origin.endsWith(".vercel.app") ||
+        origin === process.env.FRONTEND_URL
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
+  })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
